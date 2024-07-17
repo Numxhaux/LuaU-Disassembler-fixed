8887b2793e0d07e21ea43725d0e0ce07278870d1
Numxhaux <116252429+Numxhaux@users.noreply.github.com>
2024-07-17 05:36:35 -0300

    Update deserialize.lua

--- deserialize.lua
+++ deserialize.lua
@@ -1,15 +1,3 @@
---[[ 
-                                             Copyright 2024 - kkeyy
-
- All rights reserved. This Lua code is the intellectual property of kkeyy and is protected by copyright laws and international treaties. 
- Unauthorized use, reproduction, or distribution of this code, in whole or in part, without the prior written consent of kkeyy, is strictly prohibited.
- This code is provided "as is" without any warranty, express or implied, including but not limited to the implied warranties of merchantability and fitness for a particular purpose. 
- kkeyy shall not be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this code, even if advised of the possibility of such damage.
- For inquiries regarding licensing, customization, or any other use of this code, please contact kkeyy at admin@kkeyy.lol.
-
-
-]]--
-
 local function Deserialize(bytecode)
     local offset = 1
 
